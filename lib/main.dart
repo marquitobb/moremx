9a036f05b9130867ac6dacf3e2739db49165a5d1
marquitobb <maca9608@gmail.com>
2020-04-01 19:16:40 -0600

    jala bien la migraciom hasta ahorita

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:moremx/pages/splash_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      //routes: getAplicationRoutes(),
+      debugShowCheckedModeBanner: false,
+      title: 'flutter app',
+      theme: ThemeData(
+        primarySwatch: Colors.amber,
+      ),
+      home: SplashScreen(),
+    );
+  }
+}
+
